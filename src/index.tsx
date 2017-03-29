798e6182e0893f0063f5004eca8f4b0e78252e1b
Mike Koss <mike@mckoss.com>
2017-03-29 12:13:58 -0700

    React example.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Hello } from './hello';
+
 import { config } from './config';
 
 window.addEventListener('load', init);
@@ -6,6 +10,11 @@ let app: firebase.app.App;
 
 function init() {
   console.log("Application startup ...");
+
+  ReactDOM.render(
+      <Hello compiler="Typescript" framework="React" />,
+    document.getElementById('example')
+  );
 
   app = firebase.initializeApp(config);
   console.log(app);
