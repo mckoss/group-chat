3a6bcf0ecd9618292b18fc0a109f0b248eff9289
Mike Koss <mike@mckoss.com>
2017-03-29 12:56:02 -0700

    Classier.

--- src/hello.tsx
+++ src/hello.tsx
@@ -5,5 +5,12 @@ export interface HelloProps {
   framework: string;
 }
 
-export const Hello = (props: HelloProps) =>
-  <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export class Hello extends React.Component<HelloProps, undefined> {
+  render() {
+    return (
+      <h1>
+        Hello from {this.props.compiler} and {this.props.framework}!
+      </h1>
+    );
+  }
+}
