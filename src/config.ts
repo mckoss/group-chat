82493771e3943b05f432b7421070e8293085c66d
Mike Koss <mike@mckoss.com>
2017-03-28 22:58:16 -0700

    Configure for browserify and react.

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 //
 // REPLACE THE FOLLOWING LINES WITH YOUR OWN APP's CONFIGURATION
 //
-var config = {
+export const config = {
   apiKey: "AIzaSyBTEb9IWqjZtVvrhAr8jVCIkt6BtSWJjaw",
   authDomain: "koss-group-chat.firebaseapp.com",
   databaseURL: "https://koss-group-chat.firebaseio.com",
   storageBucket: "koss-group-chat.appspot.com",
   messagingSenderId: "504332387127"
 };
-firebase.initializeApp(config);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { config } from './config';
+
 window.addEventListener('load', init);
 
 let app: firebase.app.App;
@@ -5,6 +7,6 @@ let app: firebase.app.App;
 function init() {
   console.log("Application startup ...");
 
-  app = firebase.app();
+  app = firebase.initializeApp(config);
   console.log(app);
 }
